4734053de84c029ec268a2568587964dbe09b464
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2021-08-11 16:24:25 +0800

    首页

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    这是主页
+
+    <el-menu
+              :default-active="activeIndex" class="el-menu-demo"
+             mode="horizontal" @select="handleSelect">
+
+      <el-menu-item index="1">用户中心</el-menu-item>
+
+    </el-menu>
+
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+
+    }
+  }
 }
 </script>
 
-<style  lang="scss" scoped>
+<style lang="scss" scoped>
 
 </style>
